432d56d1c7aec2b91e2e8ab9f15117a3f04332a0
hanmailco34 <hanmailco34@naver.com>
2021-08-05 00:47:16 +0900

    '커밋1'

---
+++ commit.js
@@ -0,0 +1,20 @@
+var exec = require('child_process').exec;
+var argv = process.argv[2];
+
+async function run() {
+    await execFunc('git add .');
+    await execFunc(`git commit -m '${argv}'`);
+    await execFunc('git push heroku main');
+    await execFunc('git push origin main');
+}
+
+function execFunc(cmd) {
+    return new Promise((res,rej)=>{
+        exec(cmd,(err,stdout,stderr)=>{
+            if(err) console.error(error);
+            res(stdout? stdout : stderr);
+        })
+    })
+}
+
+run();
