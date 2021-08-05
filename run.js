a8f38c27d5df5b32dc4da8f112dda9becd530b45
hanmailco34 <hanmailco34@naver.com>
2021-08-05 14:49:30 +0900

    '잘못된만남2'

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const accessRefer = ['http://localhost:5000/'];
+const accessRefer = ['http://localhost:5000/','https://k-start-up.herokuapp.com/'];
 
 app.listen(port,(res,err)=>{
     console.log('home'+port);
@@ -13,7 +13,15 @@ app.use('/util',express.static('./front/util'));
 app.use('/js',express.static('./front/js'));
 
 app.use((req,res,next)=>{
-    if(accessRefer.indexOf(req.headers.referer) === -1 && req.path !== '/') {
+    console.log(req.hostname);
+    console.log(port);
+    console.log(req.protocol);
+    var hostPort = port;
+    if(hostPort !== 443 || hostPort !== 80) hostPort = ':' + hostPort;
+    const acceesHost = req.protocol + "://" + req.hostname + hostPort + '/';
+    console.log(acceesHost);
+    console.log(req.headers.referer);
+    if(acceesHost !== req.headers.referer && req.path !== '/') {
         console.log(req.path);
         res.send('잘못된 접근입니다.');
     }
