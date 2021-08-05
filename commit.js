adcd660e1ec36ca6919a5a12f478993629b02c9b
hanmailco34 <hanmailco34@naver.com>
2021-08-05 10:43:16 +0900

    '8'

--- commit.js
+++ commit.js
@@ -2,13 +2,14 @@ var exec = require('child_process').exec;
 var argv = process.argv[2];
 
 async function run() {
+    await execFunc('ssh-add ~/.ssh/linkprice');
     await execFunc('git add .');
     await execFunc(`git commit -m '${argv}'`);
     await execFunc('git push heroku master');
     await execFunc('git push origin master');
 }
 
-function execFunc(cmd) {
+function execFunc(cmd,param) {
     return new Promise((res,rej)=>{
         exec(cmd,(err,stdout,stderr)=>{
             if(err) console.error(err);
@@ -18,4 +19,4 @@ function execFunc(cmd) {
 }
 
 run();
-console.log(7);
+console.log(8);
