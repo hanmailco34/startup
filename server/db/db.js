bd9f6b1b78e16d177d404cdb2a1bc5ae11e11bdc
hanmailco34 <hanmailco34@naver.com>
2021-10-26 00:26:12 +0900

    '구조'

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,10 @@
-let { dbUrl } = require('./db_config');
+if(process.env.DATABASE_URL) {
+    let dbUrl = process.env.DATABASE_URL;
+}
+else {
+    let { dbUrl } = require('./db_config');
+}
+
 
 const { Pool } = require('pg');
 
