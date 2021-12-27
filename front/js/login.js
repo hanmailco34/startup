8cecc3abbda7c9e0ef3cd9eea967c76bdf98cbeb
hanmailco34 <hanmailco34@naver.com>
2021-12-27 23:45:31 +0900

    sdfsdfdsdf

--- front/js/login.js
+++ front/js/login.js
@@ -3,12 +3,15 @@ import rpc from './rpc';
 import '../css/login.css';
 
 $(function(){
+    $('#joinBtn').on("click",()=>{
+        common.includeHTML('join');
+    });
     var naver_client_id = 'qZEYV_9Dnf7uxDSIELMm';
     var state = common.randomString();
     var naver_redirectURI = encodeURI(rpc.snsCBUrl);
     
     var naver_api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + naver_client_id + '&redirect_uri=' + naver_redirectURI + '&state=' + state;
-    $('#naverLoginBtn').click(function() {
+    $('#naver_id_login').click(function() {
         location.href = naver_api_url
     })
 });
