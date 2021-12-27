a547aef19d5b2e71256d8f2ee2f3abbd4c53f72d
hanmailco34 <hanmailco34@naver.com>
2021-12-27 23:22:41 +0900

    구글 로그인 토큰 정보 완료

--- front/js/login.js
+++ front/js/login.js
@@ -4,11 +4,26 @@ import '../css/login.css' assert { type: "css" };
 
 $(function(){
     var naver_client_id = 'qZEYV_9Dnf7uxDSIELMm';
+    var google_client_id = '599329073244-l451mgtnohltugodvaa51j3b4o5ruhk3.apps.googleusercontent.com';
     var state = common.randomString();
-    var naver_redirectURI = encodeURI(rpc.snsCBUrl);
+    var naver_redirectURI = encodeURI(rpc.snsCBUrl + '?type=naver');
+    var gogole_auth = '';
+    var google_redirectURI = encodeURI(rpc.snsCBUrl + '?type=google');
     
     var naver_api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + naver_client_id + '&redirect_uri=' + naver_redirectURI + '&state=' + state;
+    
     $('#naverLoginBtn').click(function() {
         location.href = naver_api_url
-    })
+    });
+    $('#googleLoginBtn').click(function() {
+        gapi.load('auth2', function() {
+            gogole_auth = gapi.auth2.init({
+                client_id : google_client_id
+            });
+
+            gogole_auth.signIn().then(result => {
+                console.log(result);
+            });
+        });
+    });
 });
--- server/sns.js
+++ server/sns.js
@@ -2,59 +2,104 @@ const {db}        = require('./db/sequelize');
 const {logger}    = require('./logger');
 const request     = require('request');
 const {setToken}  = require('./token');
+const {OAuth2Client} = require('google-auth-library');
  
 module.exports = (path,app) => {
-    app.get(path+'/cb',async (req,res)=>{
-        const code = req.query.code;
-        const state = req.query.state;
-        logger.info(`ip:${req.clientIp}, code:${code}, state:${state}`);
-        var redirectURI = encodeURI(req.protocol + '//' + req.headers.host + req.url.split('?')[0]);
-        const api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
-        + process.env.NAVER_CLIENT_ID + '&client_secret=' + process.env.NAVER_CLIENT_SECRET + '&redirect_uri=' + redirectURI + '&code=' + code + '&state=' + state;
-        var options = {
-            url: api_url,
-            headers: {'X-Naver-Client-Id':process.env.NAVER_CLIENT_ID, 'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET}
-        };
-        request.get(options, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-              const token_api_url = 'https://openapi.naver.com/v1/nid/me';
-              const token = JSON.parse(body).access_token;
-              const header = "Bearer " + token;
-              const token_options = {
-                url: token_api_url,
-                headers: {'Authorization': header}
-              }
-              request.get(token_options, async function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                  const result    = JSON.parse(body).response;
-                  const sns_id    = result.id;
-                  const sns_name  = result.name;
-                  const sns_email = result.email;
-                  const sns_type  = 'naver';
-                  const f_member  = await db.Member.findMember(sns_id);
-                  logger.info(`ip:${req.clientIp}, f_member: ${f_member}, sns_id:${sns_id}, sns_type:${sns_type}, name:${sns_name}, email:${sns_email}`);
-                  if(f_member === null) {
-                    const db_res = await db.Member.joinMember(sns_id,sns_type,sns_name,sns_email);                                      
-                    if(db_res <= 0) return res.json({status:'OOPS',msg:'토큰 정보가 올바르지 않습니다.'});
-                    else return res.json({status:'OK',res:db_res.id});
-                  }
-                  else {
-                    const _info  = {id:f_member.id, sns_id:f_member.sns_id, sns_type:f_member.sns_type, name:f_member.nickname, email:f_member.email};
-                    var t = setToken(_info,res);
-                    return res.redirect('/');
-                  }                  
-                } else {
-                  console.log('error');
-                  if(response != null) {
-                    res.status(response.statusCode).end();
-                    console.log('error = ' + response.statusCode);
-                  }
-                }
-              });
-            } else {
-              res.status(response.statusCode).end();
-              console.log('error = ' + response.statusCode);
-            }
-        });
-    })
+  app.get(path+'/cb',async (req, res) => {
+    const type = req.query.type;
+    switch(type) {
+      case 'naver':
+        naverLogin(req, res);
+        break;
+      case 'google':
+        gogoleLogin(req, res);
+        break;
+    }
+  })
+}
+
+function naverLogin(req, res) {
+  const code = req.query.code;
+  const state = req.query.state;
+  logger.info(`ip:${req.clientIp}, code:${code}, state:${state}`);
+  var redirectURI = encodeURI(req.protocol + '//' + req.headers.host + req.url.split('?')[0]);
+  const api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
+  + process.env.NAVER_CLIENT_ID + '&client_secret=' + process.env.NAVER_CLIENT_SECRET + '&redirect_uri=' + redirectURI + '&code=' + code + '&state=' + state;
+  var options = {
+      url: api_url,
+      headers: {'X-Naver-Client-Id':process.env.NAVER_CLIENT_ID, 'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET}
+  };
+
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      const token_api_url = 'https://openapi.naver.com/v1/nid/me';
+      const token = JSON.parse(body).access_token;
+      const header = "Bearer " + token;
+      const token_options = {
+        url: token_api_url,
+        headers: {'Authorization': header}
+      }
+      request.get(token_options, async function (error2, response2, body2) {
+        if (!error2 && response2.statusCode == 200) {
+          getInfo(req, res, body2, 'naver');                          
+        } else {
+          console.log('error');
+          if(response2 != null) {
+            res.status(response2.statusCode).end();
+            console.log('error = ' + response2.statusCode);
+          }
+        }
+      });
+    } else {
+      res.status(response.statusCode).end();
+      console.log('error = ' + response.statusCode);
+    }
+  });
+}
+
+async function gogoleLogin(req, res) {
+  //const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+  const oAuth2Client = await getAuthenticatedClient(req);
+}
+
+function getAuthenticatedClient(req) {
+  return new Promise((resolve, reject) => {
+    // create an oAuth client to authorize the API call.  Secrets are kept in a `keys.json` file,
+    // which should be downloaded from the Google Developers Console.
+    const oAuth2Client = new OAuth2Client(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+      "http://localhost:5100/sns/cb?type=google"
+    );
+
+    // Generate the url that will be used for the consent dialog.
+    const authorizeUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: 'https://www.googleapis.com/auth/userinfo.profile',
+    });
+
+    const code = req.query.code;
+    console.log(code);
+
+  });
+}
+
+async function getInfo(req, res, body, type) {
+  const result    = JSON.parse(body).response;
+  const sns_id    = result.id;
+  const sns_name  = result.name;
+  const sns_email = result.email;
+  const sns_type  = type;
+  const f_member  = await db.Member.findMember(sns_id);
+  logger.info(`ip:${req.clientIp}, f_member: ${f_member}, sns_id:${sns_id}, sns_type:${sns_type}, name:${sns_name}, email:${sns_email}`);
+  if(f_member === null) {
+    const db_res = await db.Member.joinMember(sns_id,sns_type,sns_name,sns_email);                                      
+    if(db_res <= 0) return res.json({status:'OOPS',msg:'토큰 정보가 올바르지 않습니다.'});
+    else return res.json({status:'OK',res:db_res.id});
+  }
+  else {
+    const _info  = {id:f_member.id, sns_id:f_member.sns_id, sns_type:f_member.sns_type, name:f_member.nickname, email:f_member.email};
+    var t = setToken(_info,res);
+    return res.redirect('/');
+  }  
 }
