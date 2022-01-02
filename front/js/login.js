3b9a836e78a6ac4db2f4193a9ff2755a9a984ce4
Oscar <oscar@Oscar-M1-Pro.local>
2022-01-02 18:28:46 +0900

    카카오톡 로그인 연동 개발중

--- front/js/login.js
+++ front/js/login.js
@@ -5,12 +5,15 @@ import '../css/login.css' assert { type: "css" };
 $(function(){
     var naver_client_id = 'qZEYV_9Dnf7uxDSIELMm';
     var google_client_id = '599329073244-l451mgtnohltugodvaa51j3b4o5ruhk3.apps.googleusercontent.com';
+    var kakao_client_id = '14f2f8dfec49d3052f2c10888f4d26e9';
     var state = common.randomString();
     var naver_redirectURI = encodeURI(rpc.snsCBUrl + '?type=naver');
     var gogole_auth = '';
     var google_redirectURI = encodeURI(rpc.snsCBUrl + '?type=google');
+    var kakao_redirectURI = encodeURI(rpc.snsCBUrl + '?type=kakao');
     
     var naver_api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + naver_client_id + '&redirect_uri=' + naver_redirectURI + '&state=' + state;
+    var kakao_api_url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${kakao_client_id}&redirect_uri=${kakao_redirectURI}`;
     
     $('#naverLoginBtn').click(function() {
         location.href = naver_api_url
@@ -26,4 +29,7 @@ $(function(){
             });
         });
     });
+    $('#kakaoLoginBtn').click(function() {
+        location.href = kakao_api_url
+    })
 });
