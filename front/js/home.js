a81b771975c9c0a7d63cf6549813315731dd4a12
hanmailco34 <hanmailco34@naver.com>
2022-01-03 11:19:12 +0900

    홈화면 업로드

--- front/js/home.js
+++ front/js/home.js
@@ -2,5 +2,13 @@ import '../css/home.css' assert { type: "css" };
 import common from './common.js';
 
 $(function() {
-
+    $('.home_box').click(function() {
+        const id = $(this).data('id');
+        if(!id) {
+            common.alert('서비스 준비중','다른 서비스를 이용해주세요','info');
+        }
+        else {
+            common.includeHTML(id);
+        }
+    });
 });
